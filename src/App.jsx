cf18ca7a0bfc3bc02d03f76f3e0a79b9d12bcf8e
Reza <rezammdzd0@gmail.com>
2024-09-13 15:49:43 +0330

    feat: add hotels routs

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-
-import { Toaster } from "react-hot-toast";
-import "./App.css";
-import Header from "./components/Header/Header";
-import LocationList from "./components/LocationList/LocationList";
-import { Route, Routes } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
+import './App.css';
+import Header from './components/Header/Header';
+import LocationList from './components/LocationList/LocationList';
+import { Route, Routes } from 'react-router-dom';
+import AppLayout from './components/AppLayout/AppLayout';
 
 function App() {
-  return <div>
-    <Toaster />
-    <Header/>
-    <Routes>
-      <Route path="/" element={<LocationList />} />
-    </Routes>
-    
-  </div>
+  return (
+    <div>
+      <Toaster />
+      <Header />
+      <Routes>
+        <Route path="/" element={<LocationList />} />
+        <Route path="/hotels" element={<AppLayout />}>
+          <Route index element={<div>hotels</div>} />
+          <Route path=":id" element={<div>single hotel</div>} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
-

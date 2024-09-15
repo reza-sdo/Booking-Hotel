0f9cb7f1104fea35cf8eb763d286f08185ec0ce5
Reza <rezammdzd0@gmail.com>
2024-09-15 22:34:41 +0330

    feat: create hotles searh list

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch';
 
 const Hotels = () => {
@@ -12,14 +12,33 @@ const Hotels = () => {
   console.log(destination);
   const { isLoading, data } = useFetch(
     'http://localhost:5000/hotels',
-    `q=${
-      destination || ''
-    }&accommodates_gte=${room || 1}`
+    `q=${destination || ''}&accommodates_gte=${room || 1}`
   );
   console.log(data);
 
   if (isLoading) return <h1>loading...</h1>;
-  return <div>{data.length}</div>;
+  return (
+    <div className="searchList">
+      <h2>Search Result ({data.length})</h2>
+      {data.map((item) => (
+        <Link
+          key={item.id}
+          to={`/hotels/${item.id}?lat=${item.latitude}&lng=${item.longitude}`}
+        >
+          <div className="searchItem">
+            <img src={item.thumbnail_url} alt={item.name} />
+            <div className="searchItemDesc">
+              <p className="location">{item.smart_location}</p>
+              <p className="name">{item.name}</p>
+              <p className="price">
+                € &nbsp; {item.price} &nbsp; <span>night</span>
+              </p>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Hotels;

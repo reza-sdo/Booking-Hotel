f4b51cd5ddf0611cecc871b235115ea4b59755d0
Reza <rezammdzd0@gmail.com>
2024-09-19 20:40:15 +0330

    feat: add map t

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Map from '../Map/Map';
 
 const AppLayout = () => {
   return (
-    <div className='appLayout'>
+    <div className="appLayout">
       <div className="sidebar">
         <Outlet />
       </div>
-      <div className='mapContainer'>map</div>
+      <Map />
     </div>
-  )
-}
+  );
+};
 
-export default AppLayout
+export default AppLayout;

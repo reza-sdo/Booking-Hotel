797b62fee446ad287167c064ada48f141add921a
Reza <rezammdzd0@gmail.com>
2024-09-19 21:07:35 +0330

    feat: handle center of map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHotels } from '../context/HotelsProvider';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import { useSearchParams } from 'react-router-dom';
 
 const Map = () => {
   const { isLoading, hotels } = useHotels();
-  const [mapCenter, setMapCenter] = useState([51, 3]);
+  const [mapCenter, setMapCenter] = useState([50, 5]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
+  useEffect(() => {
+    if (lat && lng) {
+      setMapCenter([lat, lng]);
+    }
+  }, [lat, lng]);
   return (
     <div className="mapContainer">
       <MapContainer
@@ -17,6 +28,7 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
+        <ChangeCenter position={mapCenter} />
         {hotels.map((item) => (
           <Marker key={item.id} position={[item.latitude, item.longitude]}>
             <Popup>{item.host_location}</Popup>
@@ -29,3 +41,10 @@ const Map = () => {
 };
 
 export default Map;
+
+// this is for change center of map
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
